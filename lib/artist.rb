53616e993b3ff81b54d78273ad00e880b79d8d13
Ray Alva <rayalva407@gmail.com>
2020-04-07 03:23:04 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  
+  @@song_total = 1
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_total += 1
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_total += 1
+  end
+  
+  def self.song_count
+    @@song_total
+  end
+  
+end
---
+++ lib/author.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Author
+  attr_accessor :name, :posts
+  
+  @@total_posts = 1
+  
+  def initialize(name) 
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@total_posts += 1
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@total_posts += 1
+  end
+  
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+  
+  def self.post_count
+    @@total_posts
+  end
+  
+end
